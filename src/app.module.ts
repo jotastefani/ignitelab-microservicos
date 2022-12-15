0d53699c0b82056b7b8dc9f63c76a86d14776922
J.Stefani <jota.stefani@hotmail.com>
2022-12-14 22:41:08 -0300

    second leason part one

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { PrismaService } from './prisma.service';
-import { HttpModule } from './http.module';
+import { HttpModule } from './infra/http.module';
+import { PrismaService } from './infra/prisma.service';
+import { AppController } from './infra/app.controller';
 
 @Module({
   imports: [HttpModule],
